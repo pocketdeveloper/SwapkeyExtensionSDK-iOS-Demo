2962386eecbef7a965f999572be647033d3e8b08
Carolina Franco <carolina@swapme.mx>
2018-10-29 01:54:38 -0600

    feat:: Eliminando Demos anteriores

--- Demo_Swapkey_ObjC/Demo_Swapkey_ObjC/Test.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Test.swift
-//  Demo_Swapkey_ObjC
-//
-//  Created by Carolina Franco on 8/9/18.
-//  Copyright © 2018 Carolina Franco. All rights reserved.
-//
-
-import Foundation
--- Demo_Swapkey_Swift/Pods/Target Support Files/Pods-Demo_Swapkey_Swift/Pods-Demo_Swapkey_Swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Demo_Swapkey_Swift : NSObject
-@end
-@implementation PodsDummy_Pods_Demo_Swapkey_Swift
-@end
